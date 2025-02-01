bfe1c1f839f2e883890c84e342390c65d0fd5c74
loneMatterAT <luxuryacec@gmail.com>
2025-02-01 22:00:28 +0800

    This is my Javascript Series Part 7 Tasks

--- JS Tutorial #7/Challenges/Other Challenges 7.js
+++ JS Tutorial #7/Challenges/Other Challenges 7.js
@@ -1,16 +1,63 @@
 // TASK 1: LOGIN SYSTEM VALIDATOR
+{/*
+  📌 Conditions:
+      If user enter an empty username and password: "Please enter your username and password."
+      If user enter an correct username and password: "Login successful! Welcome, admin."
+      If user enter an incorrect password but username is correct: "Incorrect password. Try again."
+      If user enter an incorrect username but password is correct: "Username not recognized."
+      If user enter an incorrect username and password: "Username and paaword are incorrect."
+*/}
+// Variables
 let userName = "admin";
 let password = "1234";
 
-// Conditions
+// Conditions: Long version
 if(userName === "" || password === "") { // If user input both empty username and password
-  alert("Please enter your username and password.");
+  alert(`TASK 1: LOGIN SYSTEM VALIDATOR\nPlease enter your username and password.`);
 } else if(userName === "admin" && password === "1234") {
-  alert("Login successful! Welcome, admin.");
+  alert(`TASK 1: LOGIN SYSTEM VALIDATOR\nLogin successful! Welcome, user.`);
 } else if(userName === "admin" && password !== "1234") {
-  alert("Incorrect password. Try again.");
+  alert(`TASK 1: LOGIN SYSTEM VALIDATOR\nIncorrect password. Try again.`);
 } else if(userName !== "admin" && password === "1234") {
-  alert("Username not recognized.");
+  alert(`TASK 1: LOGIN SYSTEM VALIDATOR\nUsername not recognized.`);
 } else {
-  alert("Username and password are incorrect."); // If user input both wrong username and password
+  alert(`TASK 1: LOGIN SYSTEM VALIDATOR\nIncorrect username and password.`);
 }
+
+// Variables
+let user_id = 1001;
+let user_name = "user";
+let user_password = "12345";
+
+// Conditions: Short version
+if(user_id === "" || user_name === "" || user_password === "") alert(`TASK 1: LOGIN SYSTEM VALIDATOR\nPlease enter your user id, username and password.`);
+else if(user_id === 1001 && user_name === "user" && user_password === "12345") alert(`TASK 1: LOGIN SYSTEM VALIDATOR\nLogin successful! Welcome, user.`);
+else if(user_id !== 1001 && user_name === "user" && user_password === "12345") alert(`TASK 1: LOGIN SYSTEM VALIDATOR\nIncorrect User id. Try again.`);
+else if(user_id === 1001 && user_name !== "user" && user_password === "12345") alert(`TASK 1: LOGIN SYSTEM VALIDATOR\nUsername not recognized.`);
+else if(user_id === 1001 && user_name === "user" && user_password !== "12345") alert(`TASK 1: LOGIN SYSTEM VALIDATOR\nIncorrect password. Try again.`);
+else alert(`TASK 1: LOGIN SYSTEM VALIDATOR\nIncorrect username and password.`);
+
+
+// TASK 2: WEATHER CLOTHING DECIDER
+{/*
+  📌 Conditions:
+      If the temperature is lower than 10°C and it's raining: "Wear a heavy jacket and carry an umbrella."
+      If the temperature is lower than 10°C and it's not raining: "Wear a heavy jacket."
+      If the temperature is exactly 10°C to 25°C and it's raining: "Wear a light jacket and carry an umbrella."
+      If the temperature is exactly 10°C to 25°C and it's not raining: "Wear a light jacket."
+      If the temperature is higher than 25°C and it's raining: "Wear a t-shirt and carry an umbrella."
+      If the temperature is higher than 25°C and it's not raining: "Wear a t-shirt."  
+*/}
+// Variables
+let temperature = 100;
+let itsRaining = true;
+
+// Conditions: One liner of code
+if(temperature <= 9 && itsRaining) alert(`TASK 2: WEATHER CLOTHING DECIDER\nTemperature: ${temperature}\u00B0 C\nTip: Wear a heavy jacket and carry an umbrella.`);
+else if(temperature <= 9 && !itsRaining) alert(`TASK 2: WEATHER CLOTHING DECIDER\nTemperature: ${temperature}\u00B0 C\nTip: Wear a heavy jacket.`);
+else if(temperature <= 10 && itsRaining) alert(`TASK 2: WEATHER CLOTHING DECIDER\nTemperature: ${temperature}\u00B0 C\nTip: Wear a light jacket and carry an umbrella.`);
+else if(temperature <= 10 && !itsRaining) alert(`TASK 2: WEATHER CLOTHING DECIDER\nTemperature: ${temperature}\u00B0 C\nTip: Wear a light jacket.`);
+else if(temperature <= 25 && itsRaining) alert(`TASK 2: WEATHER CLOTHING DECIDER\nTemperature: ${temperature}\u00B0 C\nTip: Wear a t-shirt and carry an umbrella.`);
+else if(temperature <= 25 && !itsRaining) alert(`TASK 2: WEATHER CLOTHING DECIDER\nTemperature: ${temperature}\u00B0 C\nTip: Wear a t-shirt.`);
+else if (temperature <= 49 && !itsRaining) alert(`TASK 2: WEATHER CLOTHING DECIDER\nTemperature: ${temperature}\u00B0 C\nTip: Wear a Sando or Cotton t-shirt.`);
+else alert(`TASK 2: WEATHER CLOTHING DECIDER\nTemperature: ${temperature}\u00B0 C\nTip: Wear a High-end cooling suit and vest.`);
--- JS Tutorial #7/Challenges/Tutorial Challenge 7.js
+++ JS Tutorial #7/Challenges/Tutorial Challenge 7.js
@@ -14,47 +14,43 @@
 let age = 17;
 let isRegistered = false;
 // Conditions
-// if(age >= 18 && isRegistered === true) {
-//   alert("You are a Valid Voter!");
-// } else if(isRegistered === false && age >= 18) {
-//   alert("You need to Register First.")
-// } else if(isRegistered === true && age >= 17) {
-//   alert("You are Invalid Voter.")
-// } else if(isRegistered === false && age >= 17) {
-//   alert("You are a Non Voter.")
-// } else {
-//   alert("Invalid Input.")
-// }
+if(age >= 18 && isRegistered === true) {
+  alert("You are a Valid Voter!");
+} else if(isRegistered === false && age >= 18) {
+  alert("You need to Register First.")
+} else if(isRegistered === true && age >= 17) {
+  alert("You are Invalid Voter.")
+} else if(isRegistered === false && age >= 17) {
+  alert("You are a Non Voter.")
+} else {
+  alert("Invalid Input.")
+}
 
 // Second Option Using Nested Conditional Operators
-// if(age >= 18) { // 1st
-
-//   if(isRegistered) { // 3rd
-//     alert("You are a Valid Voter!");
-//   } else { // 3rd
-//     alert("You need to Register First.")
-//   }
-
-// } else if(isRegistered) { // 2nd
-//   alert("You are Invalid Voter.")
-// } else { // 1st
-//   alert("You are a Non Voter")
-// }
+if(age >= 18) { // 1st
+  if(isRegistered) { // 3rd
+    alert("You are a Valid Voter!");
+  } else { // 3rd
+    alert("You need to Register First.")
+  }
+} else if(isRegistered) { // 2nd
+  alert("You are Invalid Voter.")
+} else { // 1st
+  alert("You are a Non Voter")
+}
 
 // Mentor Solution: Using Logical Operator
-// if(isRegistered && age >= 18) alert("Valid Voter");
-// else if(!isRegistered && age >=18) alert("Register First");
-// else if(isRegistered && age < 18) alert("Invalid Voter");
-// else alert("Non Voter");
+if(isRegistered && age >= 18) alert("Valid Voter");
+else if(!isRegistered && age >=18) alert("Register First");
+else if(isRegistered && age < 18) alert("Invalid Voter");
+else alert("Non Voter");
 
 // Mentor Solution: Using Nested Conditions
 if(isRegistered) { // Group of registered
-
   if(age >= 18) alert("Valid Voter"); // Registered and 18 above
   else alert("Invalid Voter"); // Registered and below 18
 
 } else { // Group of unregistered
-
   if(age >= 18) alert("Register First"); // Not Registered and 18 above
-  else alert("Non Voter"); // Not Registered and below 18 - Non Voter
+  else alert("Non Voter"); // Not Registered and below 18 
 }
